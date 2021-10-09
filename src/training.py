ac50e89b5351aabbb4b42e0770181eb83d143d21
naganatarajan27 <nagacs@gmail.com>
2021-10-09 18:55:30 +0530

    Added for creating the Model and Plot

--- src/training.py
+++ src/training.py
@@ -1,7 +1,8 @@
 from utils.common import read_config
 from utils.data_mgmt import get_data
-from utils.model import create_model
+from utils.model import create_model, save_model, save_plot
 import argparse
+import os
 
 def training(config_path):
     config = read_config(config_path)
@@ -20,6 +21,23 @@ def training(config_path):
     VALIDATION_SET = (X_valid,y_valid)
 
     history = model.fit(X_train,y_train,epochs=EPOCHS,validation_data=VALIDATION_SET)
+
+    model_name = config["artifacts"]["model_name"]
+    model_dir = config["artifacts"]["model_dir"]
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    plots_dir = config["artifacts"]["plots_dir"]
+    plot_name = config["artifacts"]["plot_name"]
+
+    model_dir_path =os.path.join(artifacts_dir,model_dir)
+    os.makedirs(model_dir_path,exist_ok=True)
+
+    plot_dir_path = os.path.join(artifacts_dir,plots_dir)
+    os.makedirs(plot_dir_path, exist_ok=True)
+
+    save_model(model, model_name, model_dir_path)
+    
+    save_plot(history, plot_name, plot_dir_path)
+
 
 if __name__=='__main__':
     args = argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
-
+import time
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES):
 
@@ -15,3 +18,23 @@ def create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES):
     model_clf.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER, metrics=METRICS)
 
     return model_clf ## << untrained model
+
+
+def get_unique_filename(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
+
+
+def save_model(model, model_name, model_dir):
+    unique_filename = get_unique_filename(model_name)
+    path_to_model = os.path.join(model_dir, unique_filename)
+    model.save(path_to_model)
+
+
+def save_plot(history, plot_name, plot_dir):
+    pd.DataFrame(history.history).plot(figsize=(10, 7))
+    plt.grid(True)
+    unique_filename = get_unique_filename(plot_name)
+    path_to_plot = os.path.join(plot_dir, unique_filename)
+    plt.savefig(path_to_plot)
+
